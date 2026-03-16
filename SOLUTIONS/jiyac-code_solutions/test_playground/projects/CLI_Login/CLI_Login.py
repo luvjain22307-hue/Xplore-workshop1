07a62da46a644f209ad05ddf37d29736e12ec1bc
jiyac-code <jiyac1019@gmail.com>
2026-03-16 16:26:54 +0530

    Fix: Added hashing to the CLI login signup system

--- SOLUTIONS/jiyac-code_solutions/test_playground/projects/CLI_Login/CLI_Login.py
+++ SOLUTIONS/jiyac-code_solutions/test_playground/projects/CLI_Login/CLI_Login.py
@@ -1,8 +1,6 @@
 
 # Created a CLI login/signup system 
 
-<<<<<<< HEAD
-=======
 import os
 import time
 import hashlib
@@ -18,28 +16,19 @@ def hash(password):
     return hashlib.sha256(password.encode()).hexdigest()
 
 
->>>>>>> 31a6022 (Fix: Added hashing to the CLI login signup system)
 # Created a function to register the user
 def signup():
     check=False
     while not check:
-<<<<<<< HEAD
-=======
         clear_screen()
->>>>>>> 31a6022 (Fix: Added hashing to the CLI login signup system)
         print("\n---Sign Up---")
         name=input("Enter your name: ")
         password=input("Enter your password: ")
         confirm=input("Confirm password: ")
         if(password==confirm):                          # If password does match confirm password, then the user is registered
-<<<<<<< HEAD
-            with open("reg_users.txt","a") as f:
-                f.write(name+","+password+"\n")
-=======
             hashed_pass=hash(password)                  # Password is hashed and then stored
             with open("reg_users.txt","a") as f:
                 f.write(name+","+hashed_pass+"\n")
->>>>>>> 31a6022 (Fix: Added hashing to the CLI login signup system)
             print("You have successfully registered.\n")
             check=True
         else:                                           
@@ -48,29 +37,18 @@ def signup():
 
 # Created a function to login a registered user
 def login():
-<<<<<<< HEAD
-    print("\n---Login---")
-    name=input("Enter your name: ")
-    password=input("Enter your password: ")
-=======
     clear_screen()
     print("\n---Login---")
     name=input("Enter your name: ")
     password=input("Enter your password: ")
     hashed_pass=hash(password)                          # Password entered by user is hashed and compared with stored password
->>>>>>> 31a6022 (Fix: Added hashing to the CLI login signup system)
 
     with open("reg_users.txt", "r") as f:
         for line in f:
             stored_name, stored_pass=line.strip().split(",")
-<<<<<<< HEAD
-            if(stored_name==name and stored_pass==password):
-                print("You have successfully logged in!\n")
-=======
             if(stored_name==name and stored_pass==hashed_pass):
                 print("You have successfully logged in!\n")
                 clear_screen()
->>>>>>> 31a6022 (Fix: Added hashing to the CLI login signup system)
                 print("Welcome",name)
                 return True
   
@@ -83,10 +61,7 @@ def main():
     is_logged_in=False
     while choice !=3:
         if not is_logged_in:                                # Home Screen is displayed if user is not logged in
-<<<<<<< HEAD
-=======
             clear_screen()
->>>>>>> 31a6022 (Fix: Added hashing to the CLI login signup system)
             print("\n---Home---")
             print(" 1. Login","\n","2. Sign Up","\n","3. Quit")
             choice=int(input("Enter your choice: "))
